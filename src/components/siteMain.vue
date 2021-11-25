903e5509579580c122931e63390d5d0a536c92bf
ItzArima <skaypaprilia@gmail.com>
2021-11-25 13:32:15 +0100

    navbar +hero image added

--- src/components/siteMain.vue
+++ src/components/siteMain.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="main-container">
-      <h1>This is the site Main</h1>
-  </div>
+    <div class="main-container">
+        <div class="hero-container">
+            <div class="hero-image">
+                <img src="../assets/img/hero-image.png" alt="">
+            </div>
+            <div class="nav-container">
+                <div class="nav-left">
+                    <div class="logo-container">
+                        <div class="left-logo">
+                            <div class="background-logo"></div>
+                            <h1>NEX</h1>
+                        </div>
+                        <div class="right-logo">
+                            <h1>GEN</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="nav-right">
+                    <a href="#">HOME</a>
+                    <a href="#">ABOUT</a>
+                    <a href="">SERVICES</a>
+                    <a href="">PRICING</a>
+                    <a href="">BLOG</a>
+                    <button>GET IN TOUCH</button>
+                </div>    
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name:'siteMain'
-}
+    name: "siteMain",
+};
 </script>
 
 <style>
-
 </style>
