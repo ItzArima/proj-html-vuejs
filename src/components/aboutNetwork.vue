0771ffbd0a2fc2b792dbf3225b32cf6327ec7f32
ItzArima <skaypaprilia@gmail.com>
2021-11-25 18:19:30 +0100

    created aboutNetwork component

---
+++ src/components/aboutNetwork.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="about-container">
+        <div class="about-left">
+
+        </div>
+        <div class="about-right">
+            <img src="../assets/img/about-3.jpg" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/siteMain.vue
+++ src/components/siteMain.vue
@@ -2,18 +2,21 @@
     <div class="main-container">
         <hero />
         <cards1 />
+        <aboutNetwork />
     </div>
 </template>
 
 <script>
 import hero from './hero.vue'
 import cards1 from './cards-1.vue'
+import aboutNetwork from './aboutNetwork.vue'
 export default {
     name: "siteMain",
 
     components:{
         hero,
         cards1,
+        aboutNetwork
     }
 };
 </script>
