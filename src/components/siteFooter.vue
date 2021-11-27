0c9334b0c942aa1be10f8cd72e343fe4ea33be33
ItzArima <skaypaprilia@gmail.com>
2021-11-27 13:12:49 +0100

    site footer component ended: html code ended

--- src/components/siteFooter.vue
+++ src/components/siteFooter.vue
@@ -1,7 +1,139 @@
 <template>
-  <div class="footer-container">
-      <h1>This is the site Footer</h1>
-  </div>
+    <div class="footer-container">
+        <div class="footer-content">
+            <div class="content-centered">
+                <div class="footer-col">
+                    <div class="footer-title">
+                        <div class="title-left">
+                            <div class="title-bg"></div>
+                            <h1 class="hidden">NEX</h1>
+                            <h1 class="absolute">NEX</h1>
+                        </div>
+                        <div class="title-right">
+                            <h1>GEN</h1>
+                        </div>
+                    </div>    
+                    <div class="col-text">
+                        <p>A Functional HTML Template for Corporate & Business.</p>
+                    </div>
+                    <div class="contacts-container">
+                        <div class="contact">
+                            <div class="icon">
+                                <i class="fas fa-phone-alt fa-lg"></i>
+                            </div>   
+                            <p>+1 (305) 1234-5678</p>
+                        </div>
+                        <div class="contact">
+                            <div class="icon">
+                                <i class="fas fa-envelope fa-lg"></i>
+                            </div>   
+                            <p>Hello@example.com</p>
+                        </div>
+                        <div class="contact">
+                            <div class="icon">
+                                <i class="fas fa-map-marker-alt fa-lg"></i>
+                            </div>    
+                            <p>Main Avenue, 987</p>
+                        </div>
+                        <button class="btn">GET IN TOUCH</button>
+                    </div>           
+                </div>
+                <div class="footer-col">
+                    <div class="col-title"> 
+                        <h2>About</h2>
+                    </div>    
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>The Company</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Institutional</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Social & Events</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Innovation</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Environment</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Technology</p>
+                    </div>                 
+                </div>
+                <div class="footer-col">
+                    <div class="col-title"> 
+                        <h2>Services</h2>
+                    </div>    
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Audit & Assurance</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Financial Advisory</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Analytics M&A</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Middle Marketing</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Legal Consulting</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Regulatory Risk</p>
+                    </div> 
+                </div>
+                <div class="footer-col">
+                    <div class="col-title"> 
+                        <h2>Support</h2>
+                    </div>    
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Responsability</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Terms of Use</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>About Cookies</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Privacy Policy</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Accessibility</p>
+                    </div>
+                    <div class="link-container">
+                        <i class="fas fa-angle-right"></i>
+                        <p>Information</p>
+                    </div> 
+                </div>
+            </div>
+        </div>
+        <div class="footer-privacy">
+            <div class="privacy-centered">
+                <p>Enjoy the low price. We are tracking any intention of piracy.</p>
+                <p>© 2020 NEXGEN is Proudly powered by <em>Codings</em></p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
